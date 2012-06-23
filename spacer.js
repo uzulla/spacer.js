166a9daef801fd99b71801d9a9c9faf51d742108
uzulla <uzulla@himitsukichi.com>
2012-06-24 01:14:00 +0900

    add whole files, After Hachioji.pm #18

---
+++ spacer.js
@@ -0,0 +1,22 @@
+/*
+ Spacer.min.js - client side spacer.gif
+ @uzulla
+
+ require jQuery
+*/
+function setSpacer(color){
+  var canvas = $('<canvas>')[0];
+
+	$(canvas).attr("width", 1);
+	$(canvas).attr("height", 1);
+	var ctx = canvas.getContext("2d");
+	if(color){
+  	ctx.fillStyle=color;
+  	ctx.fillRect(0, 0, 1, 1);
+	}else{
+  	ctx.fillRect(0, 0, 0, 0);
+	}
+	var dataURL = canvas.toDataURL("image/png");
+	
+  $('img').filter('[src=spacer.js]').attr('src',dataURL);
+}
