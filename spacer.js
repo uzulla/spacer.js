e457c3b0057d1f1ff39f1dedaf36873a42dcd91c
Junichi Ishida <uzulla@himitsukichi.com>
2012-06-24 01:27:55 +0900

    Update master

--- spacer.js
+++ spacer.js
@@ -1,5 +1,5 @@
 /*
- Spacer.min.js - client side spacer.gif
+ Spacer.js - client side spacer.gif
  @uzulla
 
  require jQuery
@@ -7,16 +7,16 @@
 function setSpacer(color){
   var canvas = $('<canvas>')[0];
 
-	$(canvas).attr("width", 1);
-	$(canvas).attr("height", 1);
-	var ctx = canvas.getContext("2d");
-	if(color){
-  	ctx.fillStyle=color;
-  	ctx.fillRect(0, 0, 1, 1);
-	}else{
-  	ctx.fillRect(0, 0, 0, 0);
-	}
-	var dataURL = canvas.toDataURL("image/png");
+  $(canvas).attr("width", 1);
+  $(canvas).attr("height", 1);
+  var ctx = canvas.getContext("2d");
+  if(color){
+    ctx.fillStyle=color;
+    ctx.fillRect(0, 0, 1, 1);
+  }else{
+    ctx.fillRect(0, 0, 0, 0);
+  }
+  var dataURL = canvas.toDataURL("image/png");
 	
   $('img').filter('[src=spacer.js]').attr('src',dataURL);
 }
